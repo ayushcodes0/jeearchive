9c13ead8123222be1e37aa55b37c1029d6eb35b3
ayushcodes0 <ayushsinghcodes@gmail.com>
2025-07-10 19:33:18 +0530

    Some changes

--- jeearchive-admin/src/pages/User.jsx
+++ jeearchive-admin/src/pages/User.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import useUser from '../hooks/useUser';
 import "./Users.css"
 import UsersCard from '../components/UsersCard';
+// import useTest from '../hooks/useTest';
+
 
 const User = () => {
+
   const {users} = useUser();
+  // const {test} = useTest();
+  // useEffect(() => {
+  //   test
+  // }, [test])
+  
   console.log("Users: ", users)
   return (
     <div className='users-container'>
